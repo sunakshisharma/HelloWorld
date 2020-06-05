56d39ae3cb4eee85847f95db63461401a96630f1
Sunakshi <sunakshisharma197@gmail.com>
2020-06-05 11:47:04 -0400

    Database in sqlite

---
+++ ducks.py
@@ -0,0 +1,58 @@
+class Wing(object):
+
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio>1:
+            print("Weee, this is fun")
+        elif self.ratio == 1:
+            print("This is hardwork but I'm flying")
+        else:
+            print("I think i just walk")
+
+
+class Duck(object):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
+
+    def walk(self):
+        print("Waddle, waddle, waddle")
+
+    def swim(self):
+        print("Come on it, the water is lovely")
+
+    def quack(self):
+        print("Quack quack")
+
+    def fly(self):
+        self._wing.fly()
+
+
+class Penguin(object):
+    def walk(self):
+        print("Waddle, waddle, I waddle too")
+
+    def swim(self):
+        print("Come on it, but it's bit chilly this far south")
+
+    def quack(self):
+        print("Are you 'avin, a larf? I'm a penguin")
+
+
+def test_duck(duck):
+    duck.walk()
+    duck.swim()
+    duck.quack()
+
+
+if __name__ == '__main__':
+    donald = Duck()
+    donald.fly()
+    # test_duck(donald)
+
+    percy = Penguin()
+    # test_duck(percy)
+
+
